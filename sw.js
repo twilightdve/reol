2897eea7b830a3cafc83b2c98ee3fcf68821b03e
twilightea <twilightdve@gmail.com>
2026-07-01 22:26:02 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b9ecee78a9ea3525d437.js"
+    "url": "webpack-runtime-6f6cf5c960c8d9abbeb1.js"
   },
   {
     "url": "styles.9a08e8f90cabeb116238.css"
@@ -55,7 +55,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3f7df08631b4f66ed52397357e5037ae"
+    "revision": "552735746756c46454b4fc742652f978"
   },
   {
     "url": "static/webfonts/s/cormorant/v24/H4c2BXOCl9bbnla_nHIA47NMUjsNbCVrFhFTc7Nq7Q.woff2"
