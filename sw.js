2ce06c1a7a86054b2978f4f2890e878e46c10d68
twilightea <twilightdve@gmail.com>
2026-07-16 04:38:08 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-476e3b93925c8cf4d16c.js"
+    "url": "webpack-runtime-e257c83aa95a3cfbc495.js"
   },
   {
     "url": "styles.b24d0ec84b8e037866a4.css"
@@ -57,11 +57,11 @@ self.__precacheManifest = [
     "url": "0c428ae2-9a1c799a336720a8b6ba.js"
   },
   {
-    "url": "app-45846b6bbc9a64e669a4.js"
+    "url": "app-c42a00427d383dd5deaa.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "63d6dfca0a9a17989b75305064c8cd60"
+    "revision": "d8d0db6ecff6ab5436023bf6af036294"
   },
   {
     "url": "static/webfonts/s/cormorant/v24/H4c2BXOCl9bbnla_nHIA47NMUjsNbCVrFhFTc7Nq7Q.woff2"
@@ -187,7 +187,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-45846b6bbc9a64e669a4.js`))) {
+  if (!resources || !(await caches.match(`/app-c42a00427d383dd5deaa.js`))) {
     return await fetch(event.request)
   }
 
