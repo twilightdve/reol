eda6e4378a0535acf36f2c646dec40b364763a36
twilightea <twilightdve@gmail.com>
2026-07-31 03:56:35 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9acf666c8d8146540672.js"
+    "url": "webpack-runtime-d095384e60a0c1b41cab.js"
   },
   {
-    "url": "styles.80da4a9864cdf426b628.css"
+    "url": "styles.827154dcef3b2586c980.css"
   },
   {
     "url": "framework-cc19ca856aa2008e4305.js"
@@ -57,11 +57,11 @@ self.__precacheManifest = [
     "url": "0c428ae2-9a1c799a336720a8b6ba.js"
   },
   {
-    "url": "app-eb9b61dcc784a584b068.js"
+    "url": "app-8d43b146b9cda80fd1f1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8af31557b86c72df15982d1002bb7495"
+    "revision": "947422038aca4a5b308ea1e7088b8db7"
   },
   {
     "url": "static/webfonts/s/cormorant/v24/H4c2BXOCl9bbnla_nHIA47NMUjsNbCVrFhFTc7Nq7Q.woff2"
@@ -187,7 +187,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-eb9b61dcc784a584b068.js`))) {
+  if (!resources || !(await caches.match(`/app-8d43b146b9cda80fd1f1.js`))) {
     return await fetch(event.request)
   }
 
